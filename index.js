817130823812dd7a79aa3aa79c000f1bce728108
Yichuan Shen <shen.yichuan@gmail.com>
2016-08-13 14:18:48 +0200

    Add Tritonus class

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-exports.transpose = function(interval, notes) {
-    return notes.map(x => x + interval)
+let tt
+
+class Tritonus {
+    constructor(notes) {
+        this.notes = notes
+    }
+
+    transpose(interval) {
+        return tt(this.notes.map(x => x + interval))
+    }
+
+    isValid() {
+        return this.notes.every(x => Math.floor(2 * x) === Math.ceil(2 * x))
+    }
+}
+
+module.exports = tt = function(notes) {
+    return new Tritonus(notes)
+}
+
+tt.equals = function(t1, t2) {
+    if (t1.notes.length != t2.notes.length) return false
+    return t1.notes.every((n, i) => t2.notes[i] === n)
 }
--- test/spec.js
+++ test/spec.js
@@ -1,11 +1,32 @@
 const assert = require('assert')
-const tritonus = require('../index')
+const tt = require('../index')
+
+describe('isValid', function() {
+    it('should accept whole notes', function() {
+        let scale = tt([0, 1, 2, -5, -4])
+        assert(scale.isValid())
+    })
+    it('should accept semi-notes', function() {
+        let scale = tt([0, 0.5, -1.5, -2, 5.5])
+        assert(scale.isValid())
+    })
+    it('should detect rubbish entries', function() {
+        let scale = tt(['blah', 1, 2])
+        assert(!scale.isValid())
+    })
+    it('should detect non semi-notes', function() {
+        let scale = tt([0, 0.3, 0.6, 1])
+        assert(!scale.isValid())
+    })
+})
 
 describe('transpose', function() {
     it('should work', function() {
-        assert.deepEqual(
-            tritonus.transpose(2, [0, 1, 2, 3, 4, 5, 6, 7, 8]),
-            [2, 3, 4, 5, 6, 7, 8, 9, 10]
-        )
+        let scale = tt([0, 1, 2, 3, 4, 5, 6, 7, 8])
+
+        assert(tt.equals(
+            scale.transpose(2),
+            tt([2, 3, 4, 5, 6, 7, 8, 9, 10])
+        ))
     })
 })
